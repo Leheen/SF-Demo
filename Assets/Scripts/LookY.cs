7ce507e76d629f3801b027ac6c922380b5f0e51f
Hélène <h.quoitot@gmail.com>
2021-03-11 20:05:37 +0100

    Add camera movements with mouse

---
+++ Assets/Scripts/LookY.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookY : MonoBehaviour
+{
+    [SerializeField]
+    private float _sensitivity = 1f;
+
+    void Update()
+    {
+        float _mouseY = Input.GetAxis("Mouse Y");
+        Vector3 newRotation = transform.localEulerAngles;
+        newRotation.x += _mouseY * _sensitivity;
+        transform.localEulerAngles = newRotation;
+    }
+}
